435b78b0d1a544c1250068886c0011568b16e35a
nobe-s <nobe-s@hardwaru.co.jp>
2020-04-07 13:42:07 +0900

    material-ui

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {Provider} from 'react-redux'
 import thunk from "redux-thunk"
 import {BrowserRouter,Route,Switch} from 'react-router-dom'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import './index.css';
 import reducer from './reducers'
@@ -19,6 +20,7 @@ const enhancer = process.env.NODE_ENV === 'development' ?
 const store = createStore(reducer,enhancer)
 
 ReactDOM.render(
+  <MuiThemeProvider>
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
@@ -28,7 +30,8 @@ ReactDOM.render(
         <Route exact path="/" component={EventsIndex} />>
       </Switch>
     </BrowserRouter>
-  </Provider>,
+  </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 
