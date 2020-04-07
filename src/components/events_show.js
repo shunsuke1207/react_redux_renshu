af28481a72a1ab32758cabdb90873396bd5ca09b
nobe-s <nobe-s@hardwaru.co.jp>
2020-04-07 13:09:31 +0900

    imp update

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -12,6 +12,12 @@ class EventsShow extends Component {
     this.onDeleteClick = this.onDeleteClick.bind(this)
   }
 
+  componentDidMount(){
+    const {id} = this.props.match.params
+    if(id) this.props.getEvent(id)
+  }
+
+
   renderField(field) {
     const {input,label,type,meta:{touched,error}} = field
     return (
@@ -23,7 +29,7 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values){
-    //await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
 
@@ -35,7 +41,7 @@ class EventsShow extends Component {
   }
 
   render(){
-    const {handleSubmit,pristine,submitting} = this.props
+    const {handleSubmit,pristine,submitting,invalid} = this.props
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -43,7 +49,7 @@ class EventsShow extends Component {
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
 
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting} />
+          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
           <Link to="/">Cancel</Link>
           <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
         </div>
@@ -60,9 +66,12 @@ const validate = values => {
 
   return errors
 }
+const mapStateToProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id]
+  return {initialValues:event,event}
+}
+const mapDispatchToProps = ({deleteEvent,getEvent,putEvent})
 
-const mapDispatchToProps = ({deleteEvent})
-
-export default connect(null, mapDispatchToProps)(
-  reduxForm({validate,form:'eventShowForm'})(EventsShow)
+export default connect(mapStateToProps, mapDispatchToProps)(
+  reduxForm({validate,form:'eventShowForm',enableReinitialize:true})(EventsShow)
 )
